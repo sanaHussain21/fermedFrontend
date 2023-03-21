be36083fec48d8e619c3dc429de96a5d6c615541
Sana Hussain <hussainsana687@gmail.com>
2023-03-21 16:24:11 +0100

    modified some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatFormFieldModule,
     FormsModule,
     HttpClientModule,
+    MatRadioModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
