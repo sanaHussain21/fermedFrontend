43605f0d83a09d318fa24389d4f7c42fe98bde6b
Sana Hussain <hussainsana687@gmail.com>
2023-04-05 16:13:55 +0200

    modified some files

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Patient } from 'src/app/pages/patient-class/patient';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
@@ -8,11 +10,38 @@ import { Patient } from 'src/app/pages/patient-class/patient';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private observer: BreakpointObserver) { }
+
+
+
+
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+    }
+    });
+}
+
+
+
 
   ngOnInit(): void {
   }
+
+
+
+
+
+
 
 
   onLogout() {
--- src/app/pages/doctor-home/doctor-home.component.ts
+++ src/app/pages/doctor-home/doctor-home.component.ts
@@ -1,6 +1,8 @@
-import { AuthGuard } from './../../guardAuth/auth.guard';
+
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav, throwMatDuplicatedDrawerError } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-doctor-home',
@@ -9,12 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorHomeComponent implements OnInit {
   
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  constructor(private router: Router) { }
+  constructor(private observer: BreakpointObserver) { 
+    
+  }
+
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+    }
+    });
+}
+
+
+
+
 
   ngOnInit(): void {
   }
 
-  //to be implemented
+
  
 }
