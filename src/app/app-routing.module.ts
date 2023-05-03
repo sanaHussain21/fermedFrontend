7878f3cfd58694459eaa909000d6349d544bc0af
Sana Hussain <hussainsana687@gmail.com>
2023-05-03 11:22:35 +0200

    modified some files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -93,7 +93,7 @@ const routes: Routes = [
       },
 
       {
-        path: 'createAppoitment',
+        path: 'createAppointment',
         component:PatientCreateAppointmentComponent,
       },
 
