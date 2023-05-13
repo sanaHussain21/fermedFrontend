43a98a297d5fded787e0c5208f2d28b4bddfea2e
Sana Hussain <hussainsana687@gmail.com>
2023-05-13 19:41:08 +0200

    modified some file

--- src/app/pages/doctorAppointment/doctor-appointment/doctor-appointment.component.ts
+++ src/app/pages/doctorAppointment/doctor-appointment/doctor-appointment.component.ts
@@ -10,16 +10,21 @@ export class DoctorAppointmentComponent implements OnInit {
   //will have all appointment stored in this array
   appointments = [
     {
+      patientId: 56,
       payment: 100,
       datetime: '2024-12-01 00:00',
       patientName: 'Antonia Pozzi'
+      
+
     },
     {
+      patientId: 56,
       payment: 50,
       datetime: '2024-02-20 09:30:00',
       patientName: 'Antonia Pozzi'
     },
     {
+      patientId: 56,
       payment: 5,
       datetime: '2024-12-01 00:00',
       patientName: 'Antonia Pozzi'
