c8e6611f92135eaee23b65f22fdff1251a614908
Sana Hussain <hussainsana687@gmail.com>
2023-06-01 11:39:19 +0200

    created new a class for appointmentDTO

---
+++ src/app/pages/appointmentClass/appointment-class.spec.ts
@@ -0,0 +1,7 @@
+import { AppointmentClass } from './appointment-class';
+
+describe('AppointmentClass', () => {
+  it('should create an instance', () => {
+    expect(new AppointmentClass()).toBeTruthy();
+  });
+});
