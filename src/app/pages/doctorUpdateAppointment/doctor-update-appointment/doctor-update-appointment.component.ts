38f4a443eb2e40af555b34a4e63099233fba6ad6
Sana Hussain <hussainsana687@gmail.com>
2023-06-03 15:51:28 +0200

    modified some file

--- src/app/pages/doctorUpdateAppointment/doctor-update-appointment/doctor-update-appointment.component.ts
+++ src/app/pages/doctorUpdateAppointment/doctor-update-appointment/doctor-update-appointment.component.ts
@@ -6,6 +6,8 @@ import Swal from 'sweetalert2';
 import { ApplyStylesModifier } from '@popperjs/core/lib/modifiers/applyStyles';
 import { AppointmentClass } from '../../appointmentClass/appointment-class';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-doctor-update-appointment',
@@ -17,16 +19,51 @@ export class DoctorUpdateAppointmentComponent implements OnInit {
   
   appointments: AppointmentClass = new AppointmentClass();
   //[]
-
-  constructor(private _appointmentService: ViewAppointmentServiceService, private router: Router) { }
+/** */
+  editAppointment = new FormGroup({
+    patient_id: new FormControl(''),
+    time_date: new FormControl(''),
+    payment: new FormControl('')
+  })
+  
+  
+  constructor(private _appointmentService: ViewAppointmentServiceService, private router: ActivatedRoute) { }
   
  
   
-  ngOnInit(): void {
+  ngOnInit(): void { 
+   
+    console.log(this.router.snapshot.params.id_appuntamento)
+    this._appointmentService.getAppointmentById(this.router.snapshot.params.id_appuntamento).subscribe((result: any) => {
+      console.log(result)
+      this.editAppointment = new FormGroup({
+        patient_id: new FormControl(result['patient_id']),
+        time_date: new FormControl(result['time_date']),
+        payment: new FormControl(result['payment'])
+      })
+    })
+
  
   }
 
-  
+
+//update appointment 
+    updateAppointment() {
+    this._appointmentService.updateAppointment(this.router.snapshot.params.id_appuntamento, this.editAppointment.value).subscribe((result: any) => {
+      console.log(result, "appointment update successfully!! :)")
+    })
+  }
+
+
+
+  /** 
+  updateAppointment(id_appuntamento?: number) {
+    console.log(this.appointments);
+    this._appointmentService.updateAppointmentById(this.appointments).subscribe();
+    this.router.navigate(['/doctorHome/doctorAppointment'])
+    
+  }
+  */
   
   
   
--- src/app/services/view-appointment-service.service.ts
+++ src/app/services/view-appointment-service.service.ts
@@ -1,7 +1,7 @@
+import { AppointmentClass } from './../pages/appointmentClass/appointment-class';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import baseUrl from './helper';
-import { AppointmentClass } from '../pages/appointmentClass/appointment-class';
 import { Observable } from 'rxjs';
 import { TimeMaskPlaceholder } from '@syncfusion/ej2-angular-calendars';
 import { NumberInput } from '@angular/cdk/coercion';
@@ -21,23 +21,32 @@ export class ViewAppointmentServiceService {
 
  
   //update patient appointment
-  updateAppointment(id_appuntamento: number, appointments: AppointmentClass): Observable<Object>{
-    return this._httpClient.put(`${baseUrl}/appointment/updateAppointment/${id_appuntamento}`, appointments);
+  updateAppointmentById(appointments?: AppointmentClass): Observable<Object>{
+    return this._httpClient.put<Object>(`${baseUrl}/appointment/updateAppointment/${this.id_appuntamento}`, appointments);
   }
 
+  /** 
   //get id
   getId(getId_appuntamento?: number) {
     this.id_appuntamento = getId_appuntamento;
   }
 
-
+*/
 
   //get appointment by id
-  public getAppointmentById():Observable<Object> {
-    return this._httpClient.get<Object>(`${baseUrl}/appointment/getAppointmentById/${this.id_appuntamento}`);
+  public getAppointmentById(id_appuntamento: number) {
+    return this._httpClient.get(`${baseUrl}/appointment/getAppointmentById/${id_appuntamento}`);
   }
 
+  
 
-
-
+  //update appointment
+  updateAppointment(id_appuntamento: number, data: any) {
+    return this._httpClient.put(`${baseUrl}/appointment/getAppointmentById/${id_appuntamento}`,data)
+  
 }
+  
+
+
+  }
+
